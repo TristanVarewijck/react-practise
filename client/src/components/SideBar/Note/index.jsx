95c5e20a9f33677a99e4c8c2e3af9531ce27d265
TristanVarewijck <tristanconnect4@outlook.com>
2022-11-08 01:03:47 +0100

    fix: add edit and save button to frontend (sidebar note)

--- client/src/components/SideBar/Note/index.jsx
+++ client/src/components/SideBar/Note/index.jsx
@@ -1,6 +1,12 @@
+import { useRef, useState } from "react";
 import "./style.css";
 
 const Note = ({ note, setCurrentNoteId, findCurrentNote, updateNote }) => {
+  const titleInput = useRef(null);
+  const [isSubmit, setIsSubmit] = useState(true);
+
+  console.log(isSubmit);
+
   return (
     <>
       <div
@@ -15,22 +21,42 @@ const Note = ({ note, setCurrentNoteId, findCurrentNote, updateNote }) => {
             event.preventDefault();
 
             // store to localStorage
-
-            localStorage.setItem();
+            // localStorage.setItem();
           }}
         >
           <input
             name="title"
-            style={{ display: "block" }}
+            style={{ display: "block", width: "100%" }}
             className="title-input"
             type="text"
             onChange={(event) => {
               updateNote(event.target.value, "title");
             }}
+            ref={titleInput}
             value={note.title || ""}
+            disabled
           />
 
-          <input type="submit" />
+          {isSubmit ? (
+            <input
+              className="edit-title"
+              type="button"
+              onClick={() => {
+                titleInput.current.removeAttribute("disabled", "");
+                setIsSubmit(false);
+              }}
+              value="edit"
+            />
+          ) : (
+            <input
+              type="submit"
+              onClick={() => {
+                titleInput.current.setAttribute("disabled", "");
+                setIsSubmit(true);
+              }}
+              value="save"
+            />
+          )}
         </form>
       </div>
     </>
